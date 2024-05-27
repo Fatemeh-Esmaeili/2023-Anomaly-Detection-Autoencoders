0c05245be7fc12c8ee5260e50136761d31cadb9f
Fatemeh <fesm704@aucklanduni.ac.nz>
2024-05-27 16:04:42 +1200

    Data labelling for training set

---
+++ s1dataLabellingTrain.m
@@ -0,0 +1,145 @@
+%% Making Structures for Train semisupervised
+%% Data set 1: Adenosine
+
+clc; close all; clear;
+filePathLabels = "Data\labels.csv";
+SegmentLabels = readtable(filePathLabels,"PreserveVariable",true);
+
+filePathBinaryLabels = "Data\Labels2.csv";
+BinaryLabels = readtable(filePathBinaryLabels,"PreserveVariable",true);
+
+% Import Signals 
+signalsFolder = dir("Data\*-Train.csv");
+signalsTables = table({signalsFolder.name}.',{signalsFolder.folder}.','VariableNames',{'name','folder'});
+
+
+signalTitle = string(table2cell(signalsTables(:,1)));
+signalDirectories = string(table2cell(signalsTables(:,2)));
+signalDirectories = strcat(signalDirectories,'\',signalTitle);
+signalTitleList = extractBefore(signalTitle, '-Train.csv');
+signalNamePattern = "S" +  digitsPattern;
+signalID= extract(signalTitle,signalNamePattern)'
+
+
+vars = string(table2cell(SegmentLabels(:, "Channel"))')
+
+ind = [];
+for i=1: length(vars)
+    indSigLoop = strcmp(vars(i), signalID);
+    loopNonZero = find(1 == indSigLoop);
+    ind = [ind; loopNonZero];
+end
+
+
+signalTitleNew = signalTitleList(ind);
+signalsFolder = signalsFolder(ind,:);
+signalsTables = signalsTables(ind,:);
+
+% Making Structure
+% The structure seoarate each signal to different segments and put all the similar segments to a table
+analyteConcentration = ["0M","1nM","10nM","100nM","1uM","10uM"];
+
+
+s = struct;
+
+for k=1:length(analyteConcentration)
+    s(k).AC = analyteConcentration(k);    
+    s(k).DrainNA = table();
+    s(k).zscore = table();
+    s(k).SignalLabel = table();
+    s(k).SegLabel = table();
+    s(k).BiSegLabel = table();
+end
+
+vars = table2cell(SegmentLabels(:, "Channel"))';
+
+for k=1:length(analyteConcentration)
+    loopSegmentLabels = table2cell(SegmentLabels(:, k+1))';
+    loopBinaryLabels = table2cell(BinaryLabels(:, k+1))';
+
+    loopSegmentTable = cell2table(loopSegmentLabels, "VariableNames",vars);
+    loopBinaryTable = cell2table(loopBinaryLabels, "VariableNames",vars);
+
+    s(k).SegLabel = loopSegmentTable;
+    s(k).BiSegLabel = loopBinaryTable;
+end
+
+
+
+for i = 1: size(signalsTables,1)
+
+    signalTitle = string(table2cell(signalsTables(i,1)));
+    signalDirectory = string(table2cell(signalsTables(i,2)));
+    signalDirectory = strcat(signalDirectory,'\',signalTitle);
+    signalTitle = extractBefore(signalTitle, '-Train.csv');
+
+    signalNamePattern = "S" +  digitsPattern;
+    signalID= extract(signalTitle,signalNamePattern);
+    PoducerLabel = extractAfter(signalTitle, strcat(signalID,'-'));
+
+    qualityLabelPattern = signalNamePattern+"-";
+    signalSignalLabel = extract(signalTitle, qualityLabelPattern);
+    signalSignalLabel = extractAfter(signalTitle,signalSignalLabel);
+
+    T = readtable(signalDirectory,"PreserveVariableName",true);
+    con = T.C_text;    
+    concentrationSummary = groupsummary(T,"C_text");   
+
+
+    % concentration analyte CA
+    indexCA = [];
+    for l=1:length(concentrationSummary.C_text)
+        loopIndex = find(strcmp(concentrationSummary.C_text(l),analyteConcentration));
+        indexCA = [indexCA loopIndex];
+    end
+    indexCA = sort(indexCA);
+    concen = analyteConcentration(indexCA);
+
+
+
+    for k = 1: length(indexCA)
+
+        m= indexCA(k);
+        CA = s(m).AC;
+        idx = find(con == concen(k));
+        idxSize = size(idx,1);
+
+        LoopArrayRaw = T.DrainCurrentNanoA(idx);
+        LoopArrayzscore = T.zscore(idx);
+
+        LoopArraySignalLabel = PoducerLabel;
+
+
+        LoopArrayRaw = LoopArrayRaw(end-499:end);
+        LoopArrayzscore = LoopArrayzscore(end-499:end);
+
+
+        LoopTableRaw = table(LoopArrayRaw,'VariableNames',signalID);
+        LoopTablezscore = table(LoopArrayzscore,'VariableNames',signalID);
+        LoopTableSignalLabel = table(LoopArraySignalLabel, 'VariableNames', signalID);
+
+        CATableRaw = s(indexCA(k)).DrainNA;
+        CATablezscore = s(indexCA(k)).zscore;
+        CATableSignalLabel = s(indexCA(k)).SignalLabel;
+
+        newTableRaw = [CATableRaw LoopTableRaw];
+        newTablezscore = [CATablezscore  LoopTablezscore];
+        newTableSignalLabel = [CATableSignalLabel LoopTableSignalLabel];
+
+        s(m).DrainNA = newTableRaw;
+        s(m).zscore = newTablezscore;
+        s(m).SignalLabel = newTableSignalLabel;
+    end
+end
+
+s(2).SegLabel = removevars(s(2).SegLabel, 'S013');
+ s(2).BiSegLabel = removevars(s(2).BiSegLabel, 'S013');
+
+% Save Structure 
+filepath = "DataTrain";
+structureName = strcat(filepath,'\','1AdeTrainSemiSuperised','.mat');
+save(structureName,'s');
+
+
+
+
